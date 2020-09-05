29376b2e861369a74b6f01f946ae8f1b3a90fc4c
brendanlaptop <brendan@thinkitdata.com>
2020-09-05 02:23:04 -0400

    add resize and rainbow color functionality

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
-// for(i = 0; i < 16; i++){
-//     for(j = 0; j < 16; j++){
-//         $("#gameBox").append("<div class=sketch></div>")
-//     }   
-// }
-
-for(j = 0; j < 256; j++){
+for(i = 0; i < 256; i++){
     $("#gameBox").append("<div class=sketch></div>")
 }   
-
-
 
 $("#resetBtn").click(function(){
     resetGame();
 });
 
+$("#changeGrid").click(function(){
+    let area = $("#gridArea").val();
+    $(".sketch").remove();
+    for(i = 0; i < (area * area); i++){
+        $("#gameBox").append("<div class=sketch></div>")
+    }
+    $(".sketch").hover(function(){
+        $(this).addClass("darken");
+    });
+    // $("#gameBox").css({"grid-template-columns": "repeat (" + area + ", auto)", "grid-template-rows": "repeat (" + area + ", auto)" });
+    document.querySelector("#gameBox").style.gridTemplateColumns = `repeat(${area}, 1fr)`;;
+    console.log(area);
+});
+
 $(".sketch").hover(function(){
-    $(this).addClass("darken");
+    let color = $("#color").val();
+    if(color == "greyscale"){
+        $(this).addClass("darken");
+    }
+    else if(color == "rainbow"){
+        $(this).addClass("darken");
+        randomColor();
+    }
 });
 
 function resetGame(){
     $(".sketch").removeClass("darken");
+    console.log("Reset");
+}
+
+function randomColor(){
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    $(".darken").css({"background": "rgb(" + r + ", " + g + ", " + b + ")"})
 }
